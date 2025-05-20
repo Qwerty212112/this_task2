fad1bab2f52b4f0cf892877ba30c21661ca8ea40
Qwerty212112 <87649037+Qwerty212112@users.noreply.github.com>
2025-05-20 21:35:42 +0800

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,27 @@
+const dog = {
+    name: 'Чарли',
+    type: 'Собака',
+    makeSound() {
+       return 'Гав-Гав';
+    }
+ }
+ 
+ const bird = {
+    name: 'Петя',
+    type: 'Воробей',
+    makeSound() {
+       return 'Чик-чирик';
+    }
+ }
+
+ function makeDomestic (isDomestic) {
+ this.isDomestic = isDomestic
+ console.log(this)
+ console.log(`${this.type} по имени ${this.name} говорит ${this.makeSound()}`)
+ }
+
+const newFunction = makeDomestic.bind(dog,true)
+newFunction()
+
+makeDomestic.call(bird,false)
+makeDomestic.apply(bird,[false])
